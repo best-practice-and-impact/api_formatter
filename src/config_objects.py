eb67f1a61decec484a9b845ac3a364b93859e6a3
Jakke98 <jak.kempster@hotmail.co.uk>
2025-04-02 15:05:15 +0100

    Started to write getters and setters for all of the attributes of the dataset config class.

--- src/config_objects.py
+++ src/config_objects.py
@@ -6,23 +6,60 @@ class datasetConfig:
     def __init__(self, id: str, type: DatasetType, title: str, description: str, topics: list[str],
                  next_release: str, keywords: list[str], QMI_href: str, contact_name: str, contact_email: str,
                  contact_telephone: str, publisher_name: str, publisher_href: str):
-        self.id: str = id
-        self.type: DatasetType = DatasetType(type)
-        self.title: str = title
-        self.description: str = description
+        self._id: str = id
+        self._type: DatasetType = DatasetType(type)
+        self._title: str = title
+        self._description: str = description
         
-        self.topics: list[str] = topics
-        self.license: str = "Open Government License v3.0"
+        self._topics: list[str] = topics
+        self._license: str = "Open Government License v3.0"
         
-        self.next_release: str = next_release
-        self.keywords: list[str] = keywords
+        self._next_release: str = next_release
+        self._keywords: list[str] = keywords
         
-        self.QMI: dict = {"href": QMI_href}
-        self.Contact: dict = {"name": contact_name, 
+        self._QMI: dict = {"href": QMI_href}
+        self._Contact: dict = {"name": contact_name, 
                               "email": contact_email,
                               "telephone": contact_telephone}
-        self.Publisher: dict = {"name": publisher_name,
+        self._Publisher: dict = {"name": publisher_name,
                                 "href": publisher_href}
+        
+        def get_id(self):
+            return self._id
+        
+        def set_id(self, new_id):
+            self._id = new_id
+            
+        def get_type(self):
+            return self._type
+        
+        def set_type(self, new_type):
+            self._type = new_type
+            
+        def get_title(self):
+            return self._title
+        
+        def set_title(self, new_title):
+            self._title = new_title
+            
+        def get_description(self):
+            return self._description
+        
+        def set_description(self, new_description):
+            self._description = new_description
+            
+        def get_topics(self):
+            return self._topics
+        
+        def set_topics(self, new_topics):
+            self._topics = new_topics
+            
+        def get_license(self):
+            return self._license
+        
+        def set_license(self, new_license):
+            self._license = new_license
+            
         
     def __str__(self):
         return f'Dataset: {self.title}, ID: {self.id}'
@@ -38,7 +75,7 @@ class editionConfig:
         self.dataset_id: str = dataset_id
         self.edition: str = edition
         self.edition_title: str = edition_title
-        self.release_data: datetime = release_date
+        self.release_date: datetime = release_date
         
         self.version: int = version
         self.last_updated: datetime = last_updated
